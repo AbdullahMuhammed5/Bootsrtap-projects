b6ead4897f20fe8d1a4252431cb73a90c1c60e71
Abdullah Muhammed <abdo56668@gmail.com>
2020-01-11 18:55:57 +0200

    project completed

---
+++ Projects/Looplab Theme/gulpfile.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  browserSync = require('browser-sync').create();
+
+// Compile SCSS(SASS) files
+gulp.task('bootstrap:scss', function compileBootstrap() {
+  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss'])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css'))
+});
+
+// Compile SCSS(SASS) files
+gulp.task('scss', gulp.series('bootstrap:scss', function compileScss() {
+  return gulp.src(['./assets/scss/*.scss'])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css'))
+}));
+
+// Copy third party libraries from node_modules into /vendor
+gulp.task('vendor:js', function() {
+  return gulp.src([
+    './node_modules/bootstrap/dist/js/*',
+    './node_modules/jquery/dist/*',
+    '!./node_modules/jquery/dist/core.js',
+    './node_modules/popper.js/dist/umd/popper.*'
+  ])
+    .pipe(gulp.dest('./assets/js/vendor'));
+});
+
+// Configure the browserSync task and watch file path for change
+gulp.task('dev', function browserDev() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  gulp.watch(['assets/scss/*.scss','assets/scss/**/*.scss','!assets/scss/bootstrap/**'], gulp.series('scss'));
+});
---
+++ README.md
@@ -0,0 +1,9 @@
+# 5 Templete built with bootstrap 4
+These are 5 projects built with bootstrap 4,sass and gulp 4, along with [Udemy] Bootstrap 4 from Scratch with 5 Projects by Brad Traversy. 
+
+## Templetes
+1. Looplap Social Theme.
+2. Mizuxe Book Theme.
+3. Glozzom Multi Page Theme.
+4. Blogin Admin UI.
+5. Portfoligrid.
